10dbe66a672a66c126c9027a9f17da33c7c7f69f
artuino0 <arturosistemas94@gmail.com>
2022-08-24 22:56:14 -0500

    Cambio en geolocalizacion
    cambio en geolocalizacion con callbacks, estos ejecutan getWeather directo y pasan como query la latitud y longitud actual

--- app/app.js
+++ app/app.js
@@ -1,25 +1,29 @@
 var app = new Vue({
   el: "#app",
   data: {
-    apiUrl: "https://api.weatherapi.com/v1/",
-    endPoint: "",
-    apoKey: "key=4e42d51c7463474d87932720222906",
+    apiUrl: "http://api.weatherapi.com/v1/",
+    endPoint: "forecast.json?",
+    apiKey: "key=4e42d51c7463474d87932720222906&",
     queryApi: "",
+    forecastDays: 5,
   },
   methods: {
     getCurrentLocation: async function () {
       if (!navigator.geolocation) {
         throw new Error(`Your browser doesn't support Geolocation`);
       }
-      await navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          this.queryApi = `${latitude},${longitude}`;
-        },
-        (err) => {
-          console.log(`Ha ocurrido un error:`, err);
-        }
-      );
+      navigator.geolocation.getCurrentPosition(this.successGeoloaction);
     },
+    successGeoloaction: function (position) {
+      queryApi = `${position.coords.latitude},${position.coords.longitude}`;
+      this.getWeather(queryApi);
+    },
+    getWeather: function (query) {
+      let url = `${this.apiUrl}${this.endPoint}${this.apiKey}q=${query}&days=${this.forecastDays}`;
+      console.log(url);
+    },
+  },
+  mounted: function () {
+    this.getCurrentLocation();
   },
 });
