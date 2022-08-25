448200e7681f8406bae3e0f8ac6d1527cfab1498
artuino0 <arturosistemas94@gmail.com>
2022-08-24 22:11:04 -0500

    Prueba de geolocalizacion
    Hacemos un test de localizacion con navigator geolocation
    y asignamos las variables a nuestro query

--- app/app.js
+++ app/app.js
@@ -1,11 +1,25 @@
 var app = new Vue({
   el: "#app",
   data: {
-    message: "Hello Vue!",
+    apiUrl: "https://api.weatherapi.com/v1/",
+    endPoint: "",
+    apoKey: "key=4e42d51c7463474d87932720222906",
+    queryApi: "",
   },
   methods: {
-    holaMundo: function (params) {
-      alert("Hola");
+    getCurrentLocation: async function () {
+      if (!navigator.geolocation) {
+        throw new Error(`Your browser doesn't support Geolocation`);
+      }
+      await navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          this.queryApi = `${latitude},${longitude}`;
+        },
+        (err) => {
+          console.log(`Ha ocurrido un error:`, err);
+        }
+      );
     },
   },
 });
